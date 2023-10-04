dc4c1ae03a12a67aedfdb9da5683446e9a673519
593275 <69849645+593275@users.noreply.github.com>
2023-10-04 21:14:29 +0200

    test

--- python/linux_scripts/linux_scripts.py
+++ python/linux_scripts/linux_scripts.py
@@ -67,12 +67,15 @@ def start_servers(amount, amount_unpaused):
     first_server = amount_unpaused +1
     server_to_start ="openstack server unpause "
 
-    for i  in range(first_server, amount+1):
+    for i in range(first_server, amount+1):
 
         string ="Server"
         num = i
         server_name = string + str(num)+" "
         server_to_start += server_name
+
+    print(amount)
+    print(amount_unpaused)
 
     try:
         # Run the command
--- test.py
+++ test.py
@@ -1,65 +1,15 @@
-import subprocess
-import json
+def start_servers(amount, amount_unpaused):
 
-def server_list(status):
-    # Create an empty list to store server information
-    server_info_list = []
+    first_server = amount_unpaused +1
+    server_to_start ="openstack server unpause "
 
-    # Define the command to list servers
-    command = "openstack server list --format json"
+    for i  in range(first_server, amount+1):
 
-    # Use subprocess.run to run the command and capture its output
-    result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
+        string ="Server"
+        num = i
+        server_name = string + str(num)+" "
+        server_to_start += server_name
+    
+    return server_to_start
 
-    # Check if the command was successful
-    if result.returncode == 0:
-        try:
-            # Parse JSON output
-            server_data = json.loads(result.stdout)
-            for server_json in server_data:
-                server_id = server_json['ID']
-                server_name = server_json['Name']
-                server_status = server_json['Status']
-
-                if server_status == status:   
-                    server_info_list.append({"ID": server_id, "Name": server_name, "Status": server_status})
-        except json.JSONDecodeError as e:
-            print(f"Error parsing JSON: {e}")
-    else:
-        # Print any error messages if the command failed
-        print(f"Command failed with error: {result.stderr}")
-
-    # Print the stored server information
-    return server_info_list
-
-def all_server_list():
-    # Create an empty list to store server information
-    server_info_list = []
-
-    # Define the command to list servers
-    command = "openstack server list --format json"
-
-    # Use subprocess.run to run the command and capture its output
-    result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
-
-    # Check if the command was successful
-    if result.returncode == 0:
-        try:
-            # Parse JSON output
-            server_data = json.loads(result.stdout)
-            for server_json in server_data:
-                server_id = server_json['ID']
-                server_name = server_json['Name']
-                server_status = server_json['Status']
-  
-                server_info_list.append({"ID": server_id, "Name": server_name, "Status": server_status})
-        except json.JSONDecodeError as e:
-            print(f"Error parsing JSON: {e}")
-    else:
-        # Print any error messages if the command failed
-        print(f"Command failed with error: {result.stderr}")
-
-    # Print the stored server information
-    return server_info_list
-
-print(server_list("ACTIVE"))
+print(start_servers(6, 1))
