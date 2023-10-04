70ccb9fd34c59a0a21f28592c5f22172a7bc2cd1
593275 <69849645+593275@users.noreply.github.com>
2023-10-04 20:39:21 +0200

    mer bugggggggs

--- test.py
+++ test.py
@@ -1,15 +1,65 @@
-def start_servers(amount, amount_unpaused):
+import subprocess
+import json
 
-    first_server = amount_unpaused +1
-    server_to_start ="openstack server unpause "
+def server_list(status):
+    # Create an empty list to store server information
+    server_info_list = []
 
-    for i  in range(first_server, amount+1):
+    # Define the command to list servers
+    command = "openstack server list --format json"
 
-        string ="Server"
-        num = i
-        server_name = string + str(num)+" "
-        server_to_start += server_name
+    # Use subprocess.run to run the command and capture its output
+    result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
 
-    print(server_to_start)
+    # Check if the command was successful
+    if result.returncode == 0:
+        try:
+            # Parse JSON output
+            server_data = json.loads(result.stdout)
+            for server_json in server_data:
+                server_id = server_json['ID']
+                server_name = server_json['Name']
+                server_status = server_json['Status']
 
-start_servers(4, 1)
+                if server_status == status:   
+                    server_info_list.append({"ID": server_id, "Name": server_name, "Status": server_status})
+        except json.JSONDecodeError as e:
+            print(f"Error parsing JSON: {e}")
+    else:
+        # Print any error messages if the command failed
+        print(f"Command failed with error: {result.stderr}")
+
+    # Print the stored server information
+    return server_info_list
+
+def all_server_list():
+    # Create an empty list to store server information
+    server_info_list = []
+
+    # Define the command to list servers
+    command = "openstack server list --format json"
+
+    # Use subprocess.run to run the command and capture its output
+    result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
+
+    # Check if the command was successful
+    if result.returncode == 0:
+        try:
+            # Parse JSON output
+            server_data = json.loads(result.stdout)
+            for server_json in server_data:
+                server_id = server_json['ID']
+                server_name = server_json['Name']
+                server_status = server_json['Status']
+  
+                server_info_list.append({"ID": server_id, "Name": server_name, "Status": server_status})
+        except json.JSONDecodeError as e:
+            print(f"Error parsing JSON: {e}")
+    else:
+        # Print any error messages if the command failed
+        print(f"Command failed with error: {result.stderr}")
+
+    # Print the stored server information
+    return server_info_list
+
+print(all_server_list())
