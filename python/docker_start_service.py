a86d0d4390061c8bd53758bda366487507af959b
593275 <69849645+593275@users.noreply.github.com>
2023-10-18 23:45:32 +0200

    docker fix

--- python/docker_start_service.py
+++ python/docker_start_service.py
@@ -11,6 +11,7 @@ container_config = {
 service_options = {
     'name': 'my_service',  # Specify the service name
     'mode': docker.types.ServiceMode('replicated', replicas=3),  # Set the service mode and number of replicas
+    'constraints': ['node.role != manager'],
 }
 
 service = client.services.create(**container_config, **service_options)
