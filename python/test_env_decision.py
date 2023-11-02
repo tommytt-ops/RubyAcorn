6d0cc8ef64cd10f7c083c2cb977147cc6ab93159
Your Name <you@example.com>
2023-11-02 13:33:05 +0100

    testy

--- python/test_env_decision.py
+++ python/test_env_decision.py
@@ -1,25 +1,17 @@
 from prometheus_client import start_http_server, generate_latest, REGISTRY, Gauge
 import time
 
-antall_server_metric = Gauge('antall_server', ['title'])
-antall_replica_metric = Gauge('antall_replica', ['title'])
+antall_server_metric = Gauge('antall_server', 'Description of antall_server', ['title'])
+antall_replica_metric = Gauge('antall_replica', 'Description of antall_replica', ['title'])
 
 start_http_server(8000)
 
 def test():
-
     antall_server = 1.0
     antall_replica = 2.0
 
     antall_server_metric.labels(title="x").set(antall_server)
     antall_replica_metric.labels(title="y").set(antall_replica)
-
-    custom_metrics = generate_latest(REGISTRY)
-
-    
-
-
-    return custom_metrics
 
 while True:
     test()
