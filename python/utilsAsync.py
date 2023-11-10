2048c66bb4b174c720f9ac36947d1c71e026a1ee
593275 <69849645+593275@users.noreply.github.com>
2023-11-10 05:21:57 +0100

    fix plz

--- python/utilsAsync.py
+++ python/utilsAsync.py
@@ -48,6 +48,6 @@ async def get_replica_count_async(game_title):
     client = docker.from_env()
     service = client.services.get(game_title)
     replicas_value = service.attrs['Spec']['Mode']['Replicated']['Replicas']
-    print(replicas_value)
     replicas = await asyncio.to_thread(replicas_value)
+    return replicas
 
