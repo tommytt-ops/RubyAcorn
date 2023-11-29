0942f80f5006c5573aa4bd93c814c2b3cfb4e7c4
Tommy Tran <totra2383@oslomet.no>
2023-11-29 19:42:40 +0000

    plz

--- python/Utils.py
+++ python/Utils.py
@@ -7,15 +7,13 @@ from linux_scripts.linux_scripts import start_servers, stop_servers
 
 def prometheus_player_count_fetch(game_title):
  
-    url = 'http://10.196.36.11/metrics'  # Replace with the URL you want to post to
-    data = {'key': 'value'}  # Replace with your data
+    url = 'http://10.196.36.11/metrics'  
+    data = {'key': 'value'}  
     response = requests.get(url, data=data)
 
     if response.status_code == 200:
-        # Split the response content into lines
+      
         lines = response.text.split('\n')
-    
-        # Iterate through the lines and filter data for the desired title
         for line in lines:
             if game_title in line:
                 match = re.search(r'\d+$', line)
@@ -51,11 +49,9 @@ def desired_instances(instance_capacity, predicted_max_hour_player_count):
 def scaler(desiered_instance, current_instances):
     
     if desiered_instance > current_instances:
-
         start_servers(desiered_instance, current_instances)
 
     elif desiered_instance < current_instances:
-
         stop_servers(desiered_instance, current_instances)
     
 
